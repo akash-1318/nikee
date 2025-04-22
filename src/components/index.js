5dc23cb926f6be09fd7982f87d2e4880aa0cbc69
Akash Sharma <akashsharma@192.168.1.9>
2025-04-22 15:34:12 +0530

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -5,11 +5,4 @@ import PopularProductCard from "./PopularProductCard";
 import ServiceCard from "./ServiceCard";
 import ReviewCard from "./ReviewCard";
 
-export {
-    Button,
-    Nav,
-    ShoeCard,
-    PopularProductCard,
-    ServiceCard,
-    ReviewCard,
-}
+export { Button, Nav, ShoeCard, PopularProductCard, ServiceCard, ReviewCard };
